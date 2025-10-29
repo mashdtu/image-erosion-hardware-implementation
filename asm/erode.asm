dc8adc041c077a06adac66cd0193434c9c7953af
mash <mathiashk8d@gmail.com>
2025-10-29 10:25:02 +0100

    modified c file to only include erode function

--- asm/erode.asm
+++ asm/erode.asm
@@ -2,7 +2,7 @@
 # Jump to instruction 4 (0x0040000C): make pixel black and skip to next pixel
 # Jump to instruction 7 (0x00400018): do nothing and skip to next pixel
 
-li      $s0,     0x0                            # Define first pixel address (0)
+li      $s0,        0x0                         # Define first pixel address (0)
 j       0x0040001C                              # Jump to instruction 8 (skip next 4 instructions)
 
                                                 # Make pixel black
@@ -10,12 +10,12 @@ j       0x0040001C                              # Jump to instruction 8 (skip ne
 move    $t0,        $s0                         # (Instruction 4) Copy pixel register in temporary $t0 register
 addi    $t0,        $t0,        0x190           # Add 400 to the current pixel (copy to output image)
 sll     $t0,        $t0,        2               # Multiply by 4 bytes (32 bits) as we store in 32 bit slots
-sw      $zero,      0($t0)                      # Make the current pixel black in the output image
+sw      $zero,      0x0($t0)                    # Make the current pixel black in the output image
 
                                                 # Check if pixel is positioned on a border
 
 addi    $s0,        $s0,        0x1             # (Instruction 7) Increment pixel memory address to next pixel
-lw      $s1,        0($s0)                      # (Instruction 8) Load the new pixel colour
+lw      $s1,        0x0($s0)                    # (Instruction 8) Load the new pixel colour
 beq     $s1,        $zero,      0x00400018      # If pixel is black, jump to instruction 7
 
 li      $s2,        0x13                        # Set register $s2 to 0x13 = 19 
@@ -38,28 +38,28 @@ beq     $s3,        $s2,        0x0040000C      # If pixel is on right border, j
 
 addu    $s4,        $s0,        $zero           # Define another pixel
 addi    $s4,        $s4,        -0x1            # Select pixel to the left
-lw      $s1,        0($s4)
+lw      $s1,        0x0($s4)
 beq     $s1,        $zero,      0x0040000C      # If left pixel is black, make current black and skip to next
 
 addi    $s4         $s4,        0x2             # Select pixel to the right
-lw      $s1,        0($s4)
+lw      $s1,        0x0($s4)
 beq     $s1,        $zero,      0x0040000C      # If right pixel is black, make current black and skip to next
 
 addi    $s4,        $s4,        -0x1            # Select pixel above
 addi    $s4,        $s4,        -0x14
-lw      $s1,        0($s4)
+lw      $s1,        0x0($s4)
 beq     $s1,        $zero,      0x0040000C      # If above pixel is black, make current black and skip to next
 
 addi    $s4,        $s4,        0x28            # Select pixel below
-lw      $s1,        0($s4)
+lw      $s1,        0x0($s4)
 beq     $s1,        $zero,      0x0040000C      # If above pixel is black, make current black and skip to next
 
                                                 # If all neighbour pixels are white, make current white
 move    $t0,        $s0                         # Copy pixel register in a temporary $t0 register
 addi    $t0,        $t0,        0x190           # Add 400 to the current pixel (copy to output image)
-sll     $t0,        $t0,        2               # Multiply by 4 bytes (32 bits) as we store in 32 bit slots
+sll     $t0,        $t0,        0x2             # Multiply by 4 bytes (32 bits) as we store in 32 bit slots
 li      $t1,        0x1                         # Define colour of the pixel (white)
-sw      $t1,        0($t0)                      # Make the current pixel white in the output image
+sw      $t1,        0x0($t0)                    # Make the current pixel white in the output image
 
 li      $t0,        0x18F                       # Set temporary register $t0 to 0x18F = 399
 bne     $s0,        $t0,        0x00400018      # For all pixels 0 - 399, jump back to instruction 7
