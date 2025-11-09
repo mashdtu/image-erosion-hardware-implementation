3e9045bf5ad3ab3e9e23cf241428e4632f7456b3
mashdtu <131277875+mashdtu@users.noreply.github.com>
2025-11-10 00:57:33 +0100

    asm invert colour and skip top and bottom

--- asm/new.asm
+++ asm/new.asm
@@ -1,10 +1,13 @@
 ADDI $r1, $r0, 255
 ADDI $r2, $r0, 20       ; skip top border
 ADDI $r3, $r0, 380      ; skip bottom border
-BEQ $r2, $r3, 9         ; change later (jump to end)
+BEQ $r2, $r3, 12         ; change later (jump to end)
 LOAD $r4, 0($r2)        ; load pixel colour as r4
+BEQ $r4, $r1, 10        ; jump to increment if pixel is white
 ADDI $r2, $r2, 400
-STORE $r4, 0($r2)
+STORE $r1, 0($r2)
 SUBI $r2, $r2, 399      ; stored value at address r2 and incremented by 1.
+JUMP 3
+ADDI $r2, $r2, 1        ; increment by 1
 JUMP 3
 END
