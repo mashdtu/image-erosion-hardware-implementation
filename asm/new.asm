2aa136d2cfc9e16a0a925acd50fe8dfb90a7c1fa
mashdtu <131277875+mashdtu@users.noreply.github.com>
2025-11-10 00:48:20 +0100

    asm update

--- asm/new.asm
+++ asm/new.asm
@@ -2,8 +2,6 @@ ADDI $r1, $r0, 255
 ADDI $r2, $r0, 20       ; skip top border
 ADDI $r3, $r0, 380      ; skip bottom border
 BEQ $r2, $r3, 8         ; change later (jump to end)
-
-
 ADDI $r2, $r2, 400
 STORE $r1, 0($r2)
 SUBI $r2, $r2, 399      ; stored value at address r2 and incremented by 1.
