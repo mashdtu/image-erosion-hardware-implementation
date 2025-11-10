6e24e7ff570fb7c9baae2370fb05b2da27bddbc8
mashdtu <131277875+mashdtu@users.noreply.github.com>
2025-11-10 02:12:35 +0100

    WORKING IMAGE EROSION!!! (IT IS 2 AM)

--- asm/new.asm
+++ asm/new.asm
@@ -1,9 +1,21 @@
 ADDI $r1, $r0, 255
 ADDI $r2, $r0, 20       ; skip top border
 ADDI $r3, $r0, 380      ; skip bottom border
-BEQ $r2, $r3, 12        ; jump to end if inside border
+BEQ $r2, $r3, 24        ; jump to end if inside border
 LOAD $r4, 0($r2)        ; load pixel colour as r4
-BEQ $r4, $r0, 10        ; jump to end if black
+BEQ $r4, $r0, 22        ; jump to increment if black
+SUBI $r5, $r2, 20       ; set r5 to pixel above
+LOAD $r4, 0($r5)        ; load above pixel colour as r4
+BEQ $r4, $r0, 22        ; jump to increment if black
+ADDI $r5, $r2, 20       ; set r5 to pixel below
+LOAD $r4, 0($r5)        ; load above pixel colour as r4
+BEQ $r4, $r0, 22        ; jump to increment if black
+SUBI $r5, $r2, 1        ; set r5 to pixel left
+LOAD $r4, 0($r5)        ; load above pixel colour as r4
+BEQ $r4, $r0, 22        ; jump to increment if black
+ADDI $r5, $r2, 1        ; set r5 to pixel right
+LOAD $r4, 0($r5)        ; load above pixel colour as r4
+BEQ $r4, $r0, 22        ; jump to increment if black
 ADDI $r2, $r2, 400
 STORE $r1, 0($r2)
 SUBI $r2, $r2, 399      ; stored value at address r2 and incremented by 1.
