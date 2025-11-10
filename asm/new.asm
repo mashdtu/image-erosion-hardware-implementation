8559e5619d2259c00d5b598f1a244f705a6a997c
mashdtu <131277875+mashdtu@users.noreply.github.com>
2025-11-10 02:00:44 +0100

    copy image

--- asm/new.asm
+++ asm/new.asm
@@ -1,9 +1,9 @@
 ADDI $r1, $r0, 255
 ADDI $r2, $r0, 20       ; skip top border
 ADDI $r3, $r0, 380      ; skip bottom border
-BEQ $r2, $r3, 12         ; change later (jump to end)
+BEQ $r2, $r3, 12        ; jump to end if inside border
 LOAD $r4, 0($r2)        ; load pixel colour as r4
-BEQ $r4, $r1, 10        ; jump to increment if pixel is white
+BEQ $r4, $r0, 10        ; jump to end if black
 ADDI $r2, $r2, 400
 STORE $r1, 0($r2)
 SUBI $r2, $r2, 399      ; stored value at address r2 and incremented by 1.
